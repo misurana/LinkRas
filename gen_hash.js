91994950ec47f7b7983ff0e243dd160637c12077
Mukesh Rana <misurana113@gmail.com>
2026-04-18 15:49:18 +0530

    Fix: Add vercel.json for SPA routing to prevent 404 on sub-pages

---
+++ gen_hash.js
@@ -0,0 +1,8 @@
+import bcrypt from 'bcryptjs';
+
+const password = 'admin123';
+const salt = bcrypt.genSaltSync(10);
+const hash = bcrypt.hashSync(password, salt);
+
+console.log('Password: ' + password);
+console.log('Valid Hash: ' + hash);
